f4434441066f22bc2ee712824cb76d43b93626bf
K-Supritha <supritharao159@gmail.com>
2020-11-26 23:37:21 +0530

    eureka commit

---
+++ FmsEurekaServer/src/main/java/com/capg/fms/FmsEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.capg.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class FmsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsEurekaServerApplication.class, args);
+	}
+
+}
