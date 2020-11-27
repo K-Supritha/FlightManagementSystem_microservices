96afb60c39ad3bc1f784e4694fbb2b01f00c6c22
K-Supritha <supritharao159@gmail.com>
2020-11-27 17:42:43 +0530

    exception commit

--- FlightService/src/main/java/com/capg/fms/fs/exception/FlightServiceExceptions.java
+++ FlightService/src/main/java/com/capg/fms/fs/exception/FlightServiceExceptions.java
@@ -1,14 +1,15 @@
 package com.capg.fms.fs.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-public class FlightServiceExceptions extends Exception 
-{
-	private static final long serialVersionUID = 1L;
-	public FlightServiceExceptions(String message)
-	{
-		super(message);
-	}
-	public FlightServiceExceptions()
-	{
-		super();
-	}
+@ControllerAdvice
+public class FlightServiceExceptions {
+	
+	@ExceptionHandler(value={FlightNotFoundException.class })
+	public ResponseEntity<Object> exception(FlightNotFoundException exception) {
+	      return new ResponseEntity<>("Flight not found", HttpStatus.NOT_FOUND);
+	   }
+	
 }
